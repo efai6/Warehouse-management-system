70bca3829abb91270788e946fce8c09b9c8b85b6
efai6 <destrdima@gmail.com>
2025-11-27 20:46:36 +0100

    minor code improvments

--- src/source.c
+++ src/source.c
@@ -27,10 +27,8 @@ int main(int argc, char** argv) {
     fvar = fopen(argv[1], "r");
     strncpy(filetype, argv[2], 3);
     while (fgets(inp, 100, fvar)){
-        
         sscanf(inp, "%6s %29s", cmd,filename);
         if (strcmp(cmd,"init") == 0){
-            int k = 0;//start  of init
             char inp[100];
             FILE *ifvar;
             ifvar = fopen(filename, "r");
@@ -41,15 +39,12 @@ int main(int argc, char** argv) {
                 sscanf(inp, "%5s %31[^\n]", objects[k].code, objects[k].name);
                 objects[k].quantity = 0;
             }
-
-        fclose(ifvar);}//end of init
+        fclose(ifvar);}
         
-
         else if (strcmp(cmd, "update") == 0) {
             char code[6];
             char zn[2];
             char num[5];
-
             FILE *ufvar;
             ufvar = fopen(filename, "r");
             fgets(inp, 100, ufvar);
@@ -64,11 +59,9 @@ int main(int argc, char** argv) {
                 }
                     else if (strcmp(zn,"-") == 0){
                         objects[k].quantity = objects[k].quantity - atoi(num);
-                }
-                
+                }     
             }
             fclose(ufvar);
-
         }
         
         else if (strcmp(cmd, "print") == 0) {
@@ -78,7 +71,6 @@ int main(int argc, char** argv) {
             else if (strcmp(filetype,"pdf") == 0){
                 print_pdf(filename, objects,objectCount);
             }
-            
         }
     }
 fclose(fvar);
