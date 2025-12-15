1caee67e4ec13d2baf17c320770d7d5d45a79845
efai6 <destrdima@gmail.com>
2025-12-15 15:58:27 +0100

    More error handling added, bug with multiple function calling fixed

--- src/source.c
+++ src/source.c
@@ -11,6 +11,8 @@ int update_objects(char filename[30]);
 struct Object objects[1000];
 int objectCount = 0;
 int res;
+int initvar;
+
 int main(int argc, char** argv) {
     char inp[100];
     char cmd[7];
@@ -25,24 +27,40 @@ int main(int argc, char** argv) {
     FILE *fvar;
     fvar = fopen(argv[1], "r");
     if(!fvar){
-        fprintf(stderr, "The input file was not found");
+        fprintf(stderr, "The input file was not found\n");
         return 1;}
     strncpy(filetype, argv[2], 3);
     while (fgets(inp, 100, fvar)){
         sscanf(inp, "%6s %29s", cmd,filename);
-        if (strcmp(cmd,"init") == 0){
+        if (strcmp(cmd,"init") == 0){   
+            if (initvar == 1){
+                fprintf(stderr, "Two init commands in the instruction file %s were given", argv[1]);
+                return 6;
+            }
             if (init_objects(filename) == 4){
-                fprintf(stderr, "Couldn't open file given for initialisation");
+                fprintf(stderr, "Couldn't open file given for initialisation\n");
                 return init_objects(filename);
             }}
         else if (strcmp(cmd, "update") == 0) {
-            if (update_objects(filename) == 1){
-                fprintf(stderr, "Couldn't open file given for update");
-                return update_objects(filename);
+            if (initvar != 1){
+                fprintf(stderr, "The update was called before init in %s\n", argv[1]);
+                return 3;
+            }
+            int resU = update_objects(filename);
+            if (resU == 1){
+                fprintf(stderr, "Couldn't open file given for update\n");
+                return resU;
+            }
+            else if (resU == 2){
+                return resU;
             }
         }
 
         else if (strcmp(cmd, "print") == 0) {
+            if (initvar != 1){
+                fprintf(stderr, "The print was called before init in %s\n", argv[1]);
+            return 3;
+            }
             if (strcmp(filetype,"txt") == 0){
                 print_txt(filename, objects,objectCount);
             }
@@ -50,10 +68,11 @@ int main(int argc, char** argv) {
                 print_pdf(filename, objects,objectCount);
             }
             else{
-                fprintf(stderr, "Unpredicted type of output file");
+                fprintf(stderr, "Unpredicted type of output file\n");
                 return 10;
             }
         }
+        else{fprintf(stderr, "The command %s was not found\n", cmd); }
     }
 fclose(fvar);
 if (res != 0)
@@ -64,20 +83,32 @@ return 0;
 }
 
 
-
 int init_objects(char filename[30]){
        char inp[100];
+       char code [6];
+       char name [32];
+       initvar = 1;
             FILE *ifvar;
             ifvar = fopen(filename, "r");
             if (!ifvar){
                 return 4;}
             fgets(inp, 100, ifvar);
             objectCount = atoi(inp);
+            int validCount = 0;
             for (int k = 0; k < objectCount; k++) {
                 fgets(inp, 100, ifvar);
-                sscanf(inp, "%5s %31[^\n]", objects[k].code, objects[k].name);
-                objects[k].quantity = 0;
+                sscanf(inp, "%5s %31[^\n]", code, name);
+                if(strlen(code) != 5){
+                    fprintf(stderr, "the %d object in the initialisation list (%s) has incorrect code given\n", k+1, filename);
+                    res++;
+                    continue;
+                }
+                strcpy(objects[validCount].code, code);
+                strcpy(objects[validCount].name, name);
+                objects[validCount].quantity = 0;
+                validCount++;
             }
+        objectCount = validCount;
         fclose(ifvar);
         return 0;
         }
@@ -102,8 +133,13 @@ int update_objects(char filename[30]){
     while (fgets(inp, 100, ufvar)) {
         sscanf(inp, "%5s %1c %4s", code, zn, num);
         int k = 0;
-        while (k < objectCount && strcmp(code, objects[k].code) != 0) {k++;}
-        apply_update_count(&objects[k], *zn, atoi(num));
+        while (strcmp(code, objects[k].code) != 0) 
+        {k++; 
+            if (k>objectCount){
+                fprintf(stderr, "the code of the object (%s) was not innitialised\n", code);
+                return 2;
+            }
+        }
         if (apply_update_count(&objects[k], *zn, atoi(num)) != 0 ){
             fprintf(stderr, "Error in update function. The code of the error is %d\n", apply_update_count(&objects[k], *zn, atoi(num)));
             res++;
