9f30d2a411db8933fd0ed12a7fb2e507f18e6d3f
efai6 <destrdima@gmail.com>
2026-01-20 14:56:04 +0100

    Bash script upgrade

--- src/makepdf.sh
+++ src/makepdf.sh
@@ -1,4 +1,15 @@
+INPUT_FILE="$1"
 
-FILENAME="$1"
-pdflatex -interaction=nonstopmode "${FILENAME}" >/dev/null 2>&1
-echo "PDF created: ${FILENAME}.pdf"
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Plik '$INPUT_FILE' nie znaleziono."
+    exit 1
+fi
+pdflatex -interaction=nonstopmode -jobname="$INPUT_FILE" "$INPUT_FILE" >/dev/null 2>&1
+
+if [ -f "${INPUT_FILE}.pdf" ]; then
+    echo "Plik został utworzony: ${INPUT_FILE}.pdf"
+    rm -f "${INPUT_FILE}.aux" "${INPUT_FILE}.log" "${INPUT_FILE}.out" "${INPUT_FILE}.toc" "${INPUT_FILE}.synctex.gz"
+else
+    echo "Nie udało się wygenerować pdf"
+    exit 1
+fi
